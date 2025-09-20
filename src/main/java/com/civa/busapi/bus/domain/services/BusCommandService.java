9f12d2c1624bd76d7b0dff58c8db1c36b737acfe
Angel Cancho <u201721995@upc.edu.pe>
2025-09-19 19:50:11 -0500

    feat(domain): add interface BusCommandService

---
+++ src/main/java/com/civa/busapi/bus/domain/services/BusCommandService.java
@@ -0,0 +1,10 @@
+﻿package com.civa.busapi.bus.domain.services;
+
+import com.civa.busapi.bus.domain.model.aggregates.Bus;
+import com.civa.busapi.bus.domain.model.commands.CreateBusCommand;
+
+import java.util.Optional;
+
+public interface BusCommandService {
+    Optional<Bus> handle(CreateBusCommand command);
+}
