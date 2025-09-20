268513862f683fbbd301c35ea0d17a654fc02292
Angel Cancho <u201721995@upc.edu.pe>
2025-09-19 19:20:33 -0500

    feat(domain): add BusBrand aggregate with name field

---
+++ src/main/java/com/civa/busapi/bus/domain/model/aggregates/BusBrand.java
@@ -0,0 +1,22 @@
+﻿package com.civa.busapi.bus.domain.model.aggregates;
+
+import com.civa.busapi.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class BusBrand extends AuditableAbstractAggregateRoot<BusBrand> {
+
+    @NotBlank
+    @Size(max = 100)
+    private String name;
+
+    public BusBrand(String name) {
+        this.name = name;
+    }
+}
