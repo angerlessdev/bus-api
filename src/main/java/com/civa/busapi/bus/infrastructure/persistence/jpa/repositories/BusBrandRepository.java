f9806ec16ff5bae5f3c15789a40d32663190c343
Angel Cancho <u201721995@upc.edu.pe>
2025-09-19 20:14:50 -0500

    feat(infrastructure): add interface BusBrandRepository

---
+++ src/main/java/com/civa/busapi/bus/infrastructure/persistence/jpa/repositories/BusBrandRepository.java
@@ -0,0 +1,9 @@
+﻿package com.civa.busapi.bus.infrastructure.persistence.jpa.repositories;
+
+import com.civa.busapi.bus.domain.model.aggregates.BusBrand;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusBrandRepository extends JpaRepository<BusBrand, Long> {
+}
