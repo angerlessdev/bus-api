70b94f6e1819e88f89d03bb5a69b03ce83e33acf
Angel Cancho <u201721995@upc.edu.pe>
2025-09-19 19:50:02 -0500

    feat(domain): add interface BusQueryService

---
+++ src/main/java/com/civa/busapi/bus/domain/services/BusQueryService.java
@@ -0,0 +1,13 @@
+﻿package com.civa.busapi.bus.domain.services;
+
+import com.civa.busapi.bus.domain.model.aggregates.Bus;
+import com.civa.busapi.bus.domain.model.queries.GetAllBusesQuery;
+import com.civa.busapi.bus.domain.model.queries.GetBusQuery;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BusQueryService {
+    Optional<Bus> handle(GetBusQuery query);
+    List<Bus> handle(GetAllBusesQuery query);
+}
