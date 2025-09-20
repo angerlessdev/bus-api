c866f36adf50157275422cd624f7262c4dfeaf2d
Angel Cancho <u201721995@upc.edu.pe>
2025-09-20 00:06:56 -0500

    refactor(domain): update BusQueryService to support pageable results

--- src/main/java/com/civa/busapi/bus/domain/services/BusQueryService.java
+++ src/main/java/com/civa/busapi/bus/domain/services/BusQueryService.java
@@ -1,13 +1,15 @@
-﻿package com.civa.busapi.bus.domain.services;
+package com.civa.busapi.bus.domain.services;
 
 import com.civa.busapi.bus.domain.model.aggregates.Bus;
 import com.civa.busapi.bus.domain.model.queries.GetAllBusesQuery;
 import com.civa.busapi.bus.domain.model.queries.GetBusQuery;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface BusQueryService {
     Optional<Bus> handle(GetBusQuery query);
-    List<Bus> handle(GetAllBusesQuery query);
+    Page<Bus> handle(GetAllBusesQuery query, Pageable pageable);
 }
