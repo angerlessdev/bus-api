21f801e310295cf85d7bd75029a6b6d86d03c445
Angel Cancho <u201721995@upc.edu.pe>
2025-09-19 19:50:47 -0500

    feat(rest): add BusResourceFromEntityAssembler

---
+++ src/main/java/com/civa/busapi/bus/interfaces/rest/transform/BusResourceFromEntityAssembler.java
@@ -0,0 +1,16 @@
+﻿package com.civa.busapi.bus.interfaces.rest.transform;
+
+import com.civa.busapi.bus.domain.model.aggregates.Bus;
+import com.civa.busapi.bus.interfaces.rest.resources.BusResource;
+
+public class BusResourceFromEntityAssembler {
+    public static BusResource toResourceFromEntity(Bus entity) {
+        return new BusResource(
+                entity.getId(),
+                entity.getBusNumber(),
+                entity.getLicensePlate(),
+                entity.getFeatures(),
+                entity.getActive(),
+                entity.getBrand().getId());
+    }
+}
