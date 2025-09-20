c3f85a8f5d6475bf78716bac47874ce5361b2ae7
Angel Cancho <u201721995@upc.edu.pe>
2025-09-19 19:49:09 -0500

    feat(rest): add record BusResource

---
+++ src/main/java/com/civa/busapi/bus/interfaces/rest/resources/BusResource.java
@@ -0,0 +1,10 @@
+﻿package com.civa.busapi.bus.interfaces.rest.resources;
+
+public record BusResource(
+        Long id,
+        String busNumber,
+        String licensePlate,
+        String features,
+        Boolean active,
+        Long busBrandId) {
+}
