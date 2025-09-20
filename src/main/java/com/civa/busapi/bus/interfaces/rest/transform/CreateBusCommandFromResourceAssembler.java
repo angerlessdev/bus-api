f8aebf30f762cfa702a459c837cbbea39d12e8cb
Angel Cancho <u201721995@upc.edu.pe>
2025-09-19 19:50:51 -0500

    feat(rest): add CreateBusCommandFromResourceAssembler

---
+++ src/main/java/com/civa/busapi/bus/interfaces/rest/transform/CreateBusCommandFromResourceAssembler.java
@@ -0,0 +1,16 @@
+﻿package com.civa.busapi.bus.interfaces.rest.transform;
+
+import com.civa.busapi.bus.domain.model.commands.CreateBusCommand;
+import com.civa.busapi.bus.interfaces.rest.resources.CreateBusResource;
+
+public class CreateBusCommandFromResourceAssembler {
+    public static CreateBusCommand toCommandFromResource(CreateBusResource resource) {
+        return new CreateBusCommand(
+                resource.busNumber(),
+                resource.licensePlate(),
+                resource.features(),
+                resource.active(),
+                resource.busBrandId()
+        );
+    }
+}
