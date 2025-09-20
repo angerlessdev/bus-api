aa5d54ec7242684b02cb83f2619ff21fd82c86aa
Angel Cancho <u201721995@upc.edu.pe>
2025-09-19 19:48:20 -0500

    feat(infrastructure): add BusRepository extending JpaRepository

---
+++ src/main/java/com/civa/busapi/bus/infrastructure/persistence/jpa/repositories/BusRepository.java
@@ -0,0 +1,9 @@
+﻿package com.civa.busapi.bus.infrastructure.persistence.jpa.repositories;
+
+import com.civa.busapi.bus.domain.model.aggregates.Bus;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BusRepository extends JpaRepository<Bus, Long> {
+}
